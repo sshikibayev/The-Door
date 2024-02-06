66a94f97ec0447ec04c76b4cc48bf32ee22d4d39
DaFaRe <89894897+sshikibayev@users.noreply.github.com>
2024-02-06 18:34:48 +0600

    Project initialization

---
+++ Source/TheDoor/TheDoor.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class TheDoor : ModuleRules
+{
+	public TheDoor(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+	}
+}
---
+++ Source/TheDoor/TheDoorGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheDoorGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATheDoorGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATheDoorGameMode();
+};
+
+
+
